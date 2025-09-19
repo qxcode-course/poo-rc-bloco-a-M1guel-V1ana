b80b5c7fb16b727c9dc464ef92e5ad4ee9f81917
Miguel <vianamiguelangelo@gmail.com>
2025-09-19 19:07:11 -0300

    atividade ok

--- poo/database/toalha/py/draft.py
+++ poo/database/toalha/py/draft.py
@@ -1,15 +1,29 @@
-class Towel:
-    def __init__(self):
-        self.color : str = ""
-        self.size : str = ""
-        self.wetness : int = 0 
+class Towell:
+    def __init__(self, color : str, size:str):
+        self.color: str = color
+        self.size: str = size
+        self.wetness: int = 0
 
-minha = Towel()
-minha.color = "rosinha "
-minha.size = "G"
+def getMaxwetness(self) -> int:
+    if self.size == "P":
+        return 10
+    if self.size == "M":
+        return 20
+    if self.size == "G":
+        return 30
+        return 0
 
-hellys = Towel()
-hellys.color = "vermelha"
-hellys.size = "M"
+def dry(self, amount: int) -> None:
+    self.wetness += amount
+    if self.wetness > self.getMaxwetness():
+        print("toalha encharcada")
+    self.wetness = self.getMaxwetness()
 
-print(f"a cor da sua toalha é {hellys.color}")
+def wringOut(self) -> None:
+    self.wetness = 0
+def isDry(self) -> bool:
+    return self.wetness == 0
+def show(self) -> None:
+    print(self)
+def __str__(self) -> str:
+    return f"{self.color} {self.size} {self.wetness}"
