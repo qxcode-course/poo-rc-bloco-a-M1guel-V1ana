9fb3204956b318b889ab0413b6fb898c1080e4a3
miguel <vianamiguelangelo@gmail.com>
2025-09-26 23:00:50 -0300

    chove chuva

---
+++ poo/database/calculadora/py/draft.py
@@ -0,0 +1,36 @@
+class Calculadora:
+    def __init__(self, display, batteryMax ):
+        self.display: float = 0.0
+        self.batteryMax: int = 0
+
+    def __str__(self) -> str:
+        return f"display = {self.display:.2f}, battery = {self.batteryMax}"
+    
+
+    def charge(self, increment: int) -> None:
+        self.batteryMax += increment 
+
+        if self.batteryMax >5:
+            self.batteryMax = 5 
+        
+
+
+def main():
+
+    calculadora: Calculadora = Calculadora("", "")
+    while True:
+
+        line: str = input()
+        print("$" +  line)
+        args: list[str] = line.split(" ")
+
+        if args[0] == "end":
+            break
+        elif args[0] == "init":
+            batteryMax: int = args[1]
+        elif args[0] == "show":
+            print(calculadora)
+        elif args[0] == "charge":
+            calculadora.charge(int(args[1]))
+
+main()
